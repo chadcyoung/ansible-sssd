1141622707109b58d63677cd9c6d0e61296d9b28
chyoung <chyoung@DC2OLAnsible.clubcorp.com>
2015-07-14 16:52:34 -0500

    updated copyFiles.sh

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,17 +1,17 @@
 #!/usr/bash
 
 	rm -r /etc/ansible/ansibleconffiles
-	ssh -t  dc2rh028tst.clubcorp.com 'mkdir ~/ansibleconffiles && 
+	ssh -t  chyoung@dc2rh028tst.clubcorp.com 'sudo rm -r ~/ansibleconffiles &&
+					mkdir ~/ansibleconffiles && 
 					sudo cp /etc/resolv.conf ~/ansibleconffiles/ &&
 					sudo cp /etc/krb5.conf ~/ansibleconffiles/ &&
 					sudo cp /etc/sssd/sssd.conf ~/ansibleconffiles/ &&
 					sudo cp /etc/samba/smb.conf ~/ansibleconffiles/ &&
 					sudo cp /etc/hosts ~/ansibleconffiles/ && 
 					sudo cp /etc/nsswitch.conf ~/ansibleconffiles/ && 
-					sudo cp /etc/root_cert.pem ~/ansibleconffiles/ && 
-					sudo cp /etc/system-auth ~/ansibleconffiles/ &&
-					sudo chown chyoung:chyoung ~/ansibleconffiles/sssd.conf &&
-					chmod 666 ~/ansibleconffiles/sssd.conf'
+					sudo cp /etc/openldap/cacerts/root_cert.pem ~/ansibleconffiles/ && 
+					sudo cp /etc/pam.d/system-auth ~/ansibleconffiles/ &&
+					sudo chmod 777 ~/ansibleconffiles/sssd.conf'
 	scp -r chyoung@dc2rh028tst.clubcorp.com:~/ansibleconffiles /etc/ansible/
 
 
